683deeadc350be2f913e3f527abe87dcee89e98b
Chilik Laub <chilikla@gmail.com>
2022-09-09 16:04:38 +0300

    github action without python

--- rollup.config.js
+++ rollup.config.js
@@ -34,23 +34,5 @@ export default [
       file: 'dist/www/yerushamayim-card.js',
     },
     external: ['https://unpkg.com/lit-element@2.0.1/lit-element.js?module']
-  },
-  {
-    input: 'src/sensor/__init__.py',
-    output: {
-      file: 'dist/custom_component/yerushamayim/__init__.py',
-    }
-  },
-  {
-    input: 'src/sensor/sensor.py',
-    output: {
-      file: 'dist/custom_component/yerushamayim/sensor.py',
-    }
-  },
-  {
-    input: 'src/sensor/manifest.json',
-    output: {
-      file: 'dist/custom_component/yerushamayim/manifest.json',
-    }
   }
 ];
