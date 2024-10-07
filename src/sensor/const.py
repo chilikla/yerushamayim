16934d847647d7d7a55f7a595f7f496d149a4dee
Chilik Laub <chilikla@gmail.com>
2024-10-07 09:07:48 +0300

    inital steps for integration upgrade

---
+++ src/sensor/const.py
@@ -0,0 +1,6 @@
+"""Constants for the Yerushamayim integration."""
+
+DOMAIN = "yerushamayim"
+SCAN_INTERVAL = 180
+URL = "https://www.02ws.co.il/"
+API = URL + "coldmeter_service.php?lang=1&json=1&cloth_type=e"
