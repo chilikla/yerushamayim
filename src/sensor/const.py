5d59344c72bd10e50fd80fe41bc9f7c02316d41e
chilikla <chilikla@gmail.com>
2024-10-07 14:39:59 +0300

    update readme with screenshot #2

--- src/sensor/const.py
+++
@@ -1,6 +0,0 @@
-"""Constants for the Yerushamayim integration."""
-
-DOMAIN = "yerushamayim"
-SCAN_INTERVAL = 180
-URL = "https://www.02ws.co.il/"
-API = URL + "coldmeter_service.php?lang=1&json=1&cloth_type=e"
