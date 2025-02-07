6902c3b1067ac0d95180be2e28ba326b4907bf47
chilikla <chilikla@gmail.com>
2025-02-07 12:50:23 +0200

    fix: temperature missing #2

--- custom_components/yerushamayim/data_coordinator.py
+++ custom_components/yerushamayim/data_coordinator.py
@@ -216,11 +216,10 @@ class YerushamayimDataCoordinator(DataUpdateCoordinator):
         for day_part in day_parts:  
             try:
                 part = forecast_line.select(".forcast_" + day_part + " .line:nth-child(1)")[0]
-                _LOGGER.debug("%s part: %s", day_part, part)
                 temp = part.select(".number")[0].get_text().strip()
                 cloth = part.select(".cloth img")[0]
                 cloth_icon = URL + cloth["src"]
-                cloth_info = cloth["title"]
+                cloth_info = cloth["alt"]
                 forecast_data.update({
                     day_part + "_temp": temp,
                     day_part + "_cloth_icon": cloth_icon,
