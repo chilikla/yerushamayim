6f51186b6d8e786d1bf4ea81df58242d41253f7a
Chilik Laub <chilikla@gmail.com>
2025-10-15 02:59:26 +0300

    fix: initial alerts support

--- custom_components/yerushamayim/const.py
+++ custom_components/yerushamayim/const.py
@@ -5,6 +5,7 @@ from datetime import timedelta
 DOMAIN = "yerushamayim"
 SCAN_INTERVAL = timedelta(seconds=180)
 URL = "https://v2013.02ws.co.il/"
-COLDMETER_API = URL + "coldmeter_service.php?lang=1&json=1&cloth_type=e"
-# REST_API = URL + "Services.php?view=now&id=0&lang=1"
 JSON_API = URL + "02wsjson.txt"
+COLDMETER_API = URL + "coldmeter_service.php?lang=1&json=1&cloth_type=e"
+ALERTS_PAGE = URL + "small/?section=alertarchive&lang=1"
+# REST_API = URL + "Services.php?view=now&id=0&lang=1"
